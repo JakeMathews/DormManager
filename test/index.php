315e9abfab2734c2e9ebbd978099346e756496a7
Jake Mathews <mathewsj2@wit.edu>
2016-02-18 14:06:04 -0500

    Finished formatting for login page

--- test/index.php
+++ test/index.php
@@ -1,32 +1,53 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+    <title>Login</title>
+    <style type="text/css">
+    .container {
+		margin-top: 5em;
+        width: 17em;
+        clear: both;
+    }
+    .container input {
+        width: 100%;
+        clear: both;
+    }
+	#submit {
+		width: 10em;  
+		height: 2em;
+		margin-bottom: 1em;
+		margin-top: 1em;
+	}
+	#margin {
+		height: 2em;
+		margin-bottom: 1em;
+		margin-top: 1em;
+	}
+	</style>
+	
+</head>
+
 <body>
 
-<h1>Dorm Master 9000</h1>
-
-<form action="index.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-
-<?php
-$hello = "hello";
-$howareyou = "how are you";
-
-class Test {
-function hello($i) {
-	global $howareyou, $hello;
-	$this->hello = $hello . ". ";
-	$this->hello .= $howareyou;
-	$this->hello = "$hello I was wondering $i";
-}
-}
-
-$test = new Test();
-$test->hello(0xA);
-echo($test->$hello . "<br>");
-var_dump($test->$hello);
-?>
+<div align="center">
+	<div class="container">
+		<h1>Dorm Maker</h1>
+		<form action="index.php" method="post">
+			<font size="4em">
+				<label>Student ID</label> <input type="text" name="id" id="margin"><br>
+				<label>Password</label> <input type="text" name="password" id = "margin"><br>
+				<input type="submit" id="submit" class="submit">
+			</font>
+		</form>
+	</div>
+	<?php
+		if($_SERVER["REQUEST_METHOD"] == "POST") {
+			echo($_POST["id"]);
+			//$name = $_POST["id"];
+			$_SERVER["REQUEST_METHOD"] = NULL;
+		}
+	?>
+</div>
 
 </body> 
